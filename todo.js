7ddf790697c4e86784c799402419d7965d1064eb
whitehyun <cx_nf@naver.com>
2021-01-16 00:55:00 +0900

    JS: Implement button

--- todo.js
+++ todo.js
@@ -5,7 +5,7 @@ const todoInput = todoForm.getElementsByTagName("input")[0];
 const todoList = document.getElementsByClassName("todoList")[0];
 
 const TODOS_LS = "todos";
-const todoArray = [];
+let todoArray = [];
 
 function saveTodos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(todoArray));
@@ -18,13 +18,32 @@ function paintTodo(text) {
   const span = document.createElement("span");
   const newID = todoArray.length + 1;
 
+  // img
   deleteImage.src = "/images/deletebutton.png";
   deleteImage.alt = "Success";
-  span.innerText = text;
+
+  // button
   deleteButton.appendChild(deleteImage);
+  deleteButton.addEventListener("click", (event) => {
+    const button = event.target.parentNode;
+    // console.dir(button);
+    const li = button.parentNode;
+    const cleanTodos = todoArray.filter((todo) => {
+      return todo.id !== parseInt(li.id);
+    });
+    todoList.removeChild(li);
+    todoArray = cleanTodos;
+    saveTodos();
+  });
+
+  // span
+  span.innerText = text;
+
+  // li
   li.id = newID;
   li.appendChild(span);
   li.appendChild(deleteButton);
+
   todoList.appendChild(li);
 
   // --Local Storage--
