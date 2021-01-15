8638783b89718cec0ab11cd52f68efd30ae0031e
whitehyun <cx_nf@naver.com>
2021-01-16 00:28:35 +0900

    JS: todo LocalStorage

--- todo.js
+++ todo.js
@@ -5,25 +5,44 @@ const todoInput = todoForm.getElementsByTagName("input")[0];
 const todoList = document.getElementsByClassName("todoList")[0];
 
 const TODOS_LS = "todos";
+const todoArray = [];
+
+function saveTodos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(todoArray));
+}
 
 function paintTodo(text) {
   const li = document.createElement("li");
   const deleteButton = document.createElement("button");
   const deleteImage = document.createElement("img");
+  const span = document.createElement("span");
+  const newID = todoArray.length + 1;
+
   deleteImage.src = "/images/deletebutton.png";
   deleteImage.alt = "Success";
-  const span = document.createElement("span");
   span.innerText = text;
   deleteButton.appendChild(deleteImage);
+  li.id = newID;
   li.appendChild(span);
   li.appendChild(deleteButton);
   todoList.appendChild(li);
+
+  // --Local Storage--
+  const todoObj = {
+    text: text,
+    id: newID,
+  };
+  todoArray.push(todoObj);
+  saveTodos();
 }
 
 function loadTodos() {
-  const todos = localStorage.getItem(TODOS_LS);
-  if (todos !== null) {
-    //   Show Todos..
+  const loadedTodos = localStorage.getItem(TODOS_LS);
+  if (loadedTodos !== null) {
+    const parsedTodos = JSON.parse(loadedTodos);
+    parsedTodos.forEach((todo) => {
+      paintTodo(todo.text);
+    });
   }
 }
 
