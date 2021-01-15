91d293037393092b01cc25228a7704379be4037d
whitehyun <cx_nf@naver.com>
2021-01-16 01:24:48 +0900

    JS: Backgrounds

---
+++ background.js
@@ -0,0 +1,21 @@
+"use strict";
+const body = document.getElementsByTagName("body")[0];
+const IMG_NUMBER = 9;
+// TODO: User can pick INU images or landscape images to button
+
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `/images/INU/INU (${imgNumber}).jpg`;
+  image.classList.add("backgroundImage");
+  body.appendChild(image);
+}
+
+function generateRandom() {
+  const number = Math.ceil(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+(() => {
+  const randomNumber = generateRandom();
+  paintImage(randomNumber);
+})();
