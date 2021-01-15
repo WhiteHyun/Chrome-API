3c7ea57bc4d6a2ac4d5c18a21d80e13799a4bc46
whitehyun <cx_nf@naver.com>
2021-01-15 23:13:18 +0900

    JS: Greeting

---
+++ greeting.js
@@ -0,0 +1,39 @@
+"use strict";
+const form = document.getElementsByClassName("form")[0];
+const input = form.getElementsByTagName("input")[0];
+const greeting = document.getElementsByClassName("greetings")[0];
+const USER_LS = "currentUser"; //LS: Local Storage
+const SHOWING_CN = "showing"; // CN: Class Name
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+  });
+}
+
+function loadName() {
+  const currentuser = localStorage.getItem(USER_LS);
+  if (currentuser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentuser);
+  }
+}
+
+(() => {
+  loadName();
+})();
