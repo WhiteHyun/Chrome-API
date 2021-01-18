abf35fbffb8961dc7152693737af39c07b136e7a
whitehyun <cx_nf@naver.com>
2021-01-18 18:44:56 +0900

    JS: Get user's coordinates

---
+++ weather.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError(position) {
+  console.log("Can't access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    //   getWeather
+  }
+}
+
+(() => {
+  loadCoords();
+})();
